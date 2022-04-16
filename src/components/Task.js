887f5d03b7e7fac446ed293aedb9c55532681c9d
Allana Evellyn <allanaevellynm@gmail.com>
2022-04-16 18:02:44 -0300

    refactor: refactored the task cards at the kanban board and styled the login page

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
+import { FaExclamationTriangle, FaExpandAlt } from 'react-icons/fa';
 
 export const Task = (props) => {
   return (
     <Draggable draggableId={props.task.id} index={props.index}>
       {(provided) => (
-        <div
-          className="container-task"
+        <div className='task-card'
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          ref={provided.innerRef}
-        >
-          {props.task.content}
+          ref={provided.innerRef}>
+          <div className='task-card-header'>
+            <div className='kb-tooltip'>
+              <FaExclamationTriangle />
+              <div className="kb-tooltip__content">
+                Urgente
+              </div>
+            </div>
+          </div>
+          <div className='task-details-container'>
+            <div className='task-details-title'>{props.task.content}</div>
+            <div className='task-details'>
+              Protótipo inicial do Kanbang no Figma para se ter uma ideia das funcionalidades que teremos nele e suas possibilidades.
+            </div>
+          </div>
+          <div className='comments-container'>
+              <FaExpandAlt />
+          </div>
         </div>
       )}
     </Draggable>
